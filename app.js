7c55cfc3f8b90f82a8d9fec5533913b249e1b6ea
philbertopia <philiphardyj@gmail.com>
2022-12-15 08:52:34 -0500

    first commit

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", function(req, res){
 })
 
 
-app.listen(4000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Server is running on port 4000")
 })
 
